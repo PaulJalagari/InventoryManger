c8c6ce56499676ed84b34f8a72528fe88a32d708
PaulJalagari <paul.jalagari@imaginea.com>
2015-09-04 10:10:54 +0530

    changes in code

--- FileCompare/src/file/comparision/FileCompare.java
+++ FileCompare/src/file/comparision/FileCompare.java
@@ -6,23 +6,25 @@ import java.util.Scanner;
 
 public class FileCompare {
 	public static void main(String a[]) throws Exception {
-		Scanner sc=new Scanner(System.in);
+		FileCompare fc=new FileCompare();
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the path of the first file");
-		Scanner s=new Scanner(new File(sc.next()));
-		ArrayList<String> list1=new ArrayList<String>();
-		while(s.hasNext()){
-			list1.add(s.next().toLowerCase());
-		}
+		Scanner s = new Scanner(new File(sc.next()));
+		ArrayList<String> list1 = new ArrayList<String>();
+		fc.fileIntoArray(s,list1);
 		System.out.println("Enter the second file path");
-		Scanner s1=new Scanner(new File(sc.next()));
-		ArrayList<String> list2=new ArrayList<String>();
-		while(s1.hasNext()){
-			list2.add(s1.next().toLowerCase());
-		}
+		Scanner s1 = new Scanner(new File(sc.next()));
+		ArrayList<String> list2 = new ArrayList<String>();
 		ArrayList<String> common = new ArrayList<String>(list1);
+		fc.fileIntoArray(s1,list2);
 		common.retainAll(list2);
-		for(int i = 0; i < common.size(); i++){
-		    System.out.println(common.get(i));
+		for (int i = 0; i < common.size(); i++) {
+			System.out.println(common.get(i));
 		}
-				}
+	}
+	private void fileIntoArray(Scanner sc1, ArrayList<String> l){
+		while (sc1.hasNext()) {
+			l.add(sc1.next().toLowerCase());
 		}
+	}
+}
